3e1b69c96262c2975a749ebd601f35d547e679c1
Danesh Madarbakus <daneshplt@hotmail.co.uk>
2022-12-26 02:31:13 +0000

    put together requests

--- src/Views/Homepage/Hompage.tsx
+++ src/Views/Homepage/Hompage.tsx
@@ -1,29 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 
-// type props = {
-//   message?: string;
-// };
+type weatherData = {
+  name: string;
+  country: string;
+  sunriseTime: string;
+  sunsetTime: string;
+  weatherDescription: string;
+  temp: number;
+};
 
 export const Homepage = (): JSX.Element => {
-  const callAPI = async () => {
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=51.5072&lon=0.1276&appid=${process.env.WEATHER_API_KEY}`
+  const [location, setLocation] = useState<string>("");
+  const [weatherData, setWeatherData] = useState<weatherData | null>(null);
+
+  const callAPI = async (setLocation: string) => {
+    const locationResponse = await fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${setLocation}&appid=${process.env.WEATHER_API_KEY}`
     );
 
-    console.log("Danesh response", response);
+    const { lat, lon, name, country } = (await locationResponse.json())[0];
+
+    const weatherResponse = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${process.env.WEATHER_API_KEY}`
+    );
+
+    const {
+      sys: { sunrise, sunset },
+      weather: [{ description }],
+      main: { temp },
+    } = await weatherResponse.json();
+
+    const sunriseTime = convertUnixToTime(sunrise);
+    const sunsetTime = convertUnixToTime(sunset);
+
+    setWeatherData({
+      name,
+      country,
+      sunriseTime,
+      sunsetTime,
+      weatherDescription: description,
+      temp,
+    });
   };
 
   return (
     <>
       <h1>Open Weather Map</h1>
+      <label htmlFor="location">Please enter a location</label>
+      <input
+        name="location"
+        type="text"
+        onChange={(e) => {
+          setLocation(e.target.value);
+        }}
+      />
 
       <button
         onClick={() => {
-          callAPI();
+          callAPI(location);
         }}
       >
         Call api
       </button>
+
+      {weatherData ? <div>hiya</div> : null}
     </>
   );
+};
+
+const convertUnixToTime = (unixTime: number): string => {
+  const date = new Date(unixTime * 1000);
+
+  const hours = date.getHours().toLocaleString("en-GB", {
+    minimumIntegerDigits: 2,
+    useGrouping: false,
+  });
+  const minutes = date.getMinutes().toLocaleString("en-GB", {
+    minimumIntegerDigits: 2,
+    useGrouping: false,
+  });
+
+  return `${hours}:${minutes}`;
 };
