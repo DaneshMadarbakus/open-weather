9eaea7554f3666407a8cddad9974623b65f20f82
Danesh Madarbakus <daneshplt@hotmail.co.uk>
2022-12-27 02:41:10 +0000

    added routing

--- src/Views/AppWrapper/AppWrapper.tsx
+++ src/Views/AppWrapper/AppWrapper.tsx
@@ -1,18 +1,31 @@
 import React from "react";
 import { Homepage } from "../Homepage/Hompage";
+import { Link, Route, Routes } from "react-router-dom";
 
 export const AppWrapper = (): JSX.Element => {
   return (
     <>
       <header>
-        <ul>
-          <li>Home</li>
-          <li>Login</li>
-          <li>Register</li>
-        </ul>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>{" "}
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav>
       </header>
       <main>
-        <Homepage />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          {/* <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} /> */}
+        </Routes>
       </main>
     </>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,14 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { AppWrapper } from "./Views/AppWrapper/AppWrapper";
+import { BrowserRouter } from "react-router-dom";
 
 const container = document.getElementById("app-root")!;
 const root = createRoot(container);
-root.render(<AppWrapper />);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AppWrapper />
+    </BrowserRouter>
+  </React.StrictMode>
+);
